2e26bda467896d1072a15dc266115b550c713d92
Ivan Ivanov <me@ivanov.pro>
2023-11-27 11:30:08 +0200

    begin move to plain jsp

--- src/main/java/org/ivanovx/bloggable/Application.java
+++ src/main/java/org/ivanovx/bloggable/Application.java
@@ -2,11 +2,14 @@ package org.ivanovx.bloggable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Application  {
+public class Application {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
--- src/main/java/org/ivanovx/bloggable/config/WebConfig.java
+++ src/main/java/org/ivanovx/bloggable/config/WebConfig.java
@@ -1,15 +1,30 @@
 package org.ivanovx.bloggable.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
-import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+//import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 @Configuration
 public class WebConfig {
+    @Value("${spring.mvc.view.prefix}")
+    private String viewPrefix;
+
+    @Value("${spring.mvc.view.suffix}")
+    private String viewSuffix;
 
     @Bean
-    public LayoutDialect layoutDialect() {
-        return new LayoutDialect();
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix(viewPrefix);
+        viewResolver.setSuffix(viewSuffix);
+
+        return viewResolver;
     }
 }
