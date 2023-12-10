decb44494e41afb29da48b712325a2f41e881736
Ivan Ivanov <me@ivanov.pro>
2023-12-10 23:31:33 +0200

    base code refactor

--- src/main/java/org/blogy/config/WebConfig.java
+++ src/main/java/org/blogy/config/WebConfig.java
@@ -1,14 +1,33 @@
 package org.blogy.config;
 
+import java.util.concurrent.Executors;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.core.task.support.TaskExecutorAdapter;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
+@EnableAsync
 @Configuration
 public class WebConfig {
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+
+    @Bean
+    public AsyncTaskExecutor applicationTaskExecutor() {
+        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+    }
+
+    @Bean
+    public TomcatProtocolHandlerCustomizer<?> protocolHandler() {
+        return protocolHandler -> {
+            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+        };
     }
 }
